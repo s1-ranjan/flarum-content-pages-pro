d4e8b5264d7d7031bb032f04619b27e89f5a8fe9
Rai <s1.ranjan@gmail.com>
2026-02-24 18:18:47 +0530

    Update Create_page
    Modify the migration so it checks first.

--- migrations/create_pages.php
+++ migrations/create_pages.php
@@ -1,20 +1,21 @@
 <?php
 
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\Builder;
 
 return [
-    'up' => function (Builder $schema) {
-        $schema->create('pages', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->string('slug')->unique();
-            $table->longText('content');
-            $table->timestamps();
-        });
+    'up' => function ($schema) {
+        if (!$schema->hasTable('pages')) {
+            $schema->create('pages', function (Blueprint $table) {
+                $table->id();
+                $table->string('title');
+                $table->string('slug')->unique();
+                $table->longText('content');
+                $table->timestamps();
+            });
+        }
     },
 
-    'down' => function (Builder $schema) {
+    'down' => function ($schema) {
         $schema->dropIfExists('pages');
     }
 ];
