5b28c905c06842f6a96695a25fec2e76d939689f
Rai <s1.ranjan@gmail.com>
2026-02-24 20:29:21 +0530

    page content

--- extend.php
+++ extend.php
@@ -6,18 +6,9 @@ use Flarum\Extend;
 use S1Ranjan\Pages\Middleware\ResolvePage;
 
 return [
-
-    (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js'),
-
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js'),
+        ->route('/{slug}', 's1ranjan.pages'),
 
     (new Extend\Middleware('forum'))
         ->add(ResolvePage::class),
-
-    (new Extend\Routes('api'))
-        ->get('/pages', 'pages.index', Api\PageController::class)
-        ->post('/pages', 'pages.create', Api\PageController::class)
-
 ];
--- src/Middleware/ResolvePage.php
+++ src/Middleware/ResolvePage.php
@@ -2,16 +2,36 @@
 
 namespace S1Ranjan\Pages\Middleware;
 
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Laminas\Diactoros\Response\HtmlResponse;
+use S1Ranjan\Pages\PageRepository;
 
 class ResolvePage implements MiddlewareInterface
 {
+    protected $pages;
+
+    public function __construct(PageRepository $pages)
+    {
+        $this->pages = $pages;
+    }
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // For now just continue request
-        return $handler->handle($request);
+        $path = trim($request->getUri()->getPath(), '/');
+
+        if (!$path) {
+            return $handler->handle($request);
+        }
+
+        $page = $this->pages->findBySlug($path);
+
+        if (!$page) {
+            return $handler->handle($request);
+        }
+
+        return new HtmlResponse($page->content);
     }
 }
