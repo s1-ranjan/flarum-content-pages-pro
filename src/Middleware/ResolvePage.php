2b96da1d6784bf0d07ee68bcf8b5df55f19fa45a
Rai <s1.ranjan@gmail.com>
2026-02-24 20:13:36 +0530

    r fix

--- src/Middleware/ResolvePage.php
+++ src/Middleware/ResolvePage.php
@@ -2,28 +2,16 @@
 
 namespace S1Ranjan\Pages\Middleware;
 
-use Laminas\Diactoros\Response\HtmlResponse;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
-use S1Ranjan\Pages\PageRepository;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class ResolvePage implements MiddlewareInterface
 {
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler)
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $path = trim($request->getUri()->getPath(), '/');
-
-        if ($path === '' || str_starts_with($path, 't/') || str_starts_with($path, 'd/')) {
-            return $handler->handle($request);
-        }
-
-        $page = resolve(PageRepository::class)->findBySlug($path);
-
-        if (!$page) {
-            return $handler->handle($request);
-        }
-
-        return new HtmlResponse($page->content);
+        // For now just continue request
+        return $handler->handle($request);
     }
 }
